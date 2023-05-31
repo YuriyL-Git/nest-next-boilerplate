8ab90d65a75884012e30632a4b1d69306b611fc7
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-05-31 21:57:50 +0300

    add home block feature

---
+++ libs/api/feature-home-block/src/lib/home-block.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@next-nest-boilerplate/api/data-access-db";
+import {
+  CreateOneHomeBlockArgs,
+  DeleteOneHomeBlockArgs,
+  FindUniqueHomeBlockArgs,
+  UpdateOneHomeBlockArgs
+} from "@next-nest-boilerplate/api/generated-db-types";
+
+@Injectable()
+export class HomeBlockService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createArgs: CreateOneHomeBlockArgs) {
+    return this.prisma.homeBlock.create({
+      data: createArgs.data,
+      include: {
+        rgbBackground: true
+      }
+    });
+  }
+
+  findAll() {
+    return this.prisma.homeBlock.findMany();
+  }
+
+  findOne(findArgs: FindUniqueHomeBlockArgs) {
+    const { where } = findArgs;
+    return this.prisma.homeBlock.findUnique({
+      where,
+      include: {
+        rgbBackground: true
+      }
+    });
+  }
+
+  update(updateArgs: UpdateOneHomeBlockArgs) {
+    const { where, data } = updateArgs;
+    return this.prisma.homeBlock.update({
+      where,
+      data,
+      include: {
+        rgbBackground: true
+      }
+    });
+  }
+
+  remove(deleteArgs: DeleteOneHomeBlockArgs) {
+    return this.prisma.homeBlock.delete({
+      where: deleteArgs.where,
+      include: {
+        rgbBackground: true
+      }
+    });
+  }
+}
--- libs/web/data-access-graphql/src/lib/generated/types.ts
+++ libs/web/data-access-graphql/src/lib/generated/types.ts
@@ -14,11 +14,78 @@ export type Scalars = {
   Float: { input: number; output: number; }
 };
 
+export type HomeBlock = {
+  __typename?: 'HomeBlock';
+  id: Scalars['ID']['output'];
+  imagePath: Scalars['String']['output'];
+  navigationPath: Scalars['String']['output'];
+  rgbBackground: RgbBackground;
+  rgbBackgroundId: Scalars['String']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type HomeBlockCountAggregate = {
+  __typename?: 'HomeBlockCountAggregate';
+  _all: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  imagePath: Scalars['Int']['output'];
+  navigationPath: Scalars['Int']['output'];
+  rgbBackgroundId: Scalars['Int']['output'];
+  title: Scalars['Int']['output'];
+};
+
+export type HomeBlockCreateInput = {
+  id?: InputMaybe<Scalars['String']['input']>;
+  imagePath: Scalars['String']['input'];
+  navigationPath: Scalars['String']['input'];
+  rgbBackground: RgbBackgroundCreateNestedOneWithoutHomeBlockInput;
+  title: Scalars['String']['input'];
+};
+
+export type HomeBlockMaxAggregate = {
+  __typename?: 'HomeBlockMaxAggregate';
+  id?: Maybe<Scalars['String']['output']>;
+  imagePath?: Maybe<Scalars['String']['output']>;
+  navigationPath?: Maybe<Scalars['String']['output']>;
+  rgbBackgroundId?: Maybe<Scalars['String']['output']>;
+  title?: Maybe<Scalars['String']['output']>;
+};
+
+export type HomeBlockMinAggregate = {
+  __typename?: 'HomeBlockMinAggregate';
+  id?: Maybe<Scalars['String']['output']>;
+  imagePath?: Maybe<Scalars['String']['output']>;
+  navigationPath?: Maybe<Scalars['String']['output']>;
+  rgbBackgroundId?: Maybe<Scalars['String']['output']>;
+  title?: Maybe<Scalars['String']['output']>;
+};
+
+export type HomeBlockUpdateInput = {
+  id?: InputMaybe<Scalars['String']['input']>;
+  imagePath?: InputMaybe<Scalars['String']['input']>;
+  navigationPath?: InputMaybe<Scalars['String']['input']>;
+  rgbBackground?: InputMaybe<RgbBackgroundUpdateOneRequiredWithoutHomeBlockNestedInput>;
+  title?: InputMaybe<Scalars['String']['input']>;
+};
+
+export type HomeBlockWhereUniqueInput = {
+  id?: InputMaybe<Scalars['String']['input']>;
+  rgbBackgroundId?: InputMaybe<Scalars['String']['input']>;
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
+  createHomeBlock: HomeBlock;
   createUser: User;
+  removeHomeBlock: HomeBlock;
   removeUser: User;
+  updateHomeBlock: HomeBlock;
   updateUser: User;
+};
+
+
+export type MutationCreateHomeBlockArgs = {
+  data: HomeBlockCreateInput;
 };
 
 
@@ -27,8 +94,19 @@ export type MutationCreateUserArgs = {
 };
 
 
+export type MutationRemoveHomeBlockArgs = {
+  where: HomeBlockWhereUniqueInput;
+};
+
+
 export type MutationRemoveUserArgs = {
   where: UserWhereUniqueInput;
+};
+
+
+export type MutationUpdateHomeBlockArgs = {
+  data: HomeBlockUpdateInput;
+  where: HomeBlockWhereUniqueInput;
 };
 
 
@@ -39,13 +117,118 @@ export type MutationUpdateUserArgs = {
 
 export type Query = {
   __typename?: 'Query';
+  HomeBlock: HomeBlock;
+  HomeBlocks: Array<HomeBlock>;
   user: User;
   users: Array<User>;
 };
 
 
+export type QueryHomeBlockArgs = {
+  where: HomeBlockWhereUniqueInput;
+};
+
+
 export type QueryUserArgs = {
   where: UserWhereUniqueInput;
+};
+
+export type RgbBackground = {
+  __typename?: 'RgbBackground';
+  a?: Maybe<Scalars['Float']['output']>;
+  b: Scalars['Int']['output'];
+  g: Scalars['Int']['output'];
+  homeBlock?: Maybe<HomeBlock>;
+  id: Scalars['ID']['output'];
+  r: Scalars['Int']['output'];
+};
+
+export type RgbBackgroundAvgAggregate = {
+  __typename?: 'RgbBackgroundAvgAggregate';
+  a?: Maybe<Scalars['Float']['output']>;
+  b?: Maybe<Scalars['Float']['output']>;
+  g?: Maybe<Scalars['Float']['output']>;
+  r?: Maybe<Scalars['Float']['output']>;
+};
+
+export type RgbBackgroundCountAggregate = {
+  __typename?: 'RgbBackgroundCountAggregate';
+  _all: Scalars['Int']['output'];
+  a: Scalars['Int']['output'];
+  b: Scalars['Int']['output'];
+  g: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  r: Scalars['Int']['output'];
+};
+
+export type RgbBackgroundCreateNestedOneWithoutHomeBlockInput = {
+  connect?: InputMaybe<RgbBackgroundWhereUniqueInput>;
+  connectOrCreate?: InputMaybe<RgbBackgroundCreateOrConnectWithoutHomeBlockInput>;
+  create?: InputMaybe<RgbBackgroundCreateWithoutHomeBlockInput>;
+};
+
+export type RgbBackgroundCreateOrConnectWithoutHomeBlockInput = {
+  create: RgbBackgroundCreateWithoutHomeBlockInput;
+  where: RgbBackgroundWhereUniqueInput;
+};
+
+export type RgbBackgroundCreateWithoutHomeBlockInput = {
+  a?: InputMaybe<Scalars['Float']['input']>;
+  b: Scalars['Int']['input'];
+  g: Scalars['Int']['input'];
+  id?: InputMaybe<Scalars['String']['input']>;
+  r: Scalars['Int']['input'];
+};
+
+export type RgbBackgroundMaxAggregate = {
+  __typename?: 'RgbBackgroundMaxAggregate';
+  a?: Maybe<Scalars['Float']['output']>;
+  b?: Maybe<Scalars['Int']['output']>;
+  g?: Maybe<Scalars['Int']['output']>;
+  id?: Maybe<Scalars['String']['output']>;
+  r?: Maybe<Scalars['Int']['output']>;
+};
+
+export type RgbBackgroundMinAggregate = {
+  __typename?: 'RgbBackgroundMinAggregate';
+  a?: Maybe<Scalars['Float']['output']>;
+  b?: Maybe<Scalars['Int']['output']>;
+  g?: Maybe<Scalars['Int']['output']>;
+  id?: Maybe<Scalars['String']['output']>;
+  r?: Maybe<Scalars['Int']['output']>;
+};
+
+export type RgbBackgroundSumAggregate = {
+  __typename?: 'RgbBackgroundSumAggregate';
+  a?: Maybe<Scalars['Float']['output']>;
+  b?: Maybe<Scalars['Int']['output']>;
+  g?: Maybe<Scalars['Int']['output']>;
+  r?: Maybe<Scalars['Int']['output']>;
+};
+
+export type RgbBackgroundUpdateOneRequiredWithoutHomeBlockNestedInput = {
+  connect?: InputMaybe<RgbBackgroundWhereUniqueInput>;
+  connectOrCreate?: InputMaybe<RgbBackgroundCreateOrConnectWithoutHomeBlockInput>;
+  create?: InputMaybe<RgbBackgroundCreateWithoutHomeBlockInput>;
+  update?: InputMaybe<RgbBackgroundUpdateWithoutHomeBlockInput>;
+  upsert?: InputMaybe<RgbBackgroundUpsertWithoutHomeBlockInput>;
+};
+
+export type RgbBackgroundUpdateWithoutHomeBlockInput = {
+  a?: InputMaybe<Scalars['Float']['input']>;
+  b?: InputMaybe<Scalars['Int']['input']>;
+  g?: InputMaybe<Scalars['Int']['input']>;
+  id?: InputMaybe<Scalars['String']['input']>;
+  r?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type RgbBackgroundUpsertWithoutHomeBlockInput = {
+  create: RgbBackgroundCreateWithoutHomeBlockInput;
+  update: RgbBackgroundUpdateWithoutHomeBlockInput;
+};
+
+export type RgbBackgroundWhereUniqueInput = {
+  id?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type User = {
