ad6fc70f9d625c8805e6e36af4d0dc4466a9ed08
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-06-02 19:57:23 +0300

    add generate public file watcher

--- tools/generate-public-files-list/generate-public-list.ts
+++ tools/generate-public-files-list/generate-public-list.ts
@@ -1,15 +1,16 @@
 const fs = require("fs").promises;
+const chokidar = require("chokidar");
 
 const encoding = "utf8";
 const pathToPublicFolder = "./apps/web/public";
 const pathToFile = "./apps/web/i18n/publicFiles.gen.json";
-
-setTimeout(async () => {
-  await generatePublicFilesList();
-}, 0);
 
 async function generatePublicFilesList() {
   const publicFiles = (await fs.readdir(pathToPublicFolder)).map((file) => `/${file}`);
   const result = JSON.stringify({ publicFiles }, null, 2);
   fs.writeFile(pathToFile, result, encoding);
 }
+
+chokidar.watch(pathToPublicFolder).on("all", () => {
+  generatePublicFilesList();
+});
