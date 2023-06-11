ff5e9b0f8a214b6662075ff2ae463273780f91b2
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-06-11 17:18:37 +0300

    setup env file check sum validation

--- tools/validate-env/validate-env.ts
+++ tools/validate-env/validate-env.ts
@@ -8,7 +8,13 @@ import {
   ValidateNested,
 } from "class-validator";
 import "reflect-metadata";
+import * as fs from "node:fs/promises";
+
+const encoding = "utf8";
 import { environment } from "../../libs/shared/environement/src/index";
+import { watch } from "chokidar";
+import { createHash } from "crypto";
+import * as process from "process";
 
 type ApiEnv = typeof environment;
 
@@ -86,10 +92,34 @@ if (errors.length) {
   process.exit(1);
 }
 
-/*
-const EnvProdFile;
+const { isProd } = environment;
 
-chokidar.watch(pathToPublicFolder).on("all", () => {
-  generatePublicFilesList();
-});
-*/
+const envPath = "./.env";
+const checkSumPath = "./tools/validate-env/env-checksum.sum";
+
+const getCheckSum = (content: string) => {
+  const cleanContent = content
+    .split("\n")
+    .map((line) => line.replace(/\\n/g, ""))
+    .filter((line) => line.trim().length > 0 && !line.trim().startsWith("#"))
+    .join("");
+
+  return createHash("md5").update(cleanContent).digest("hex");
+};
+
+if (isProd) {
+  setTimeout(async () => {
+    const content = await fs.readFile(envPath, encoding);
+    const checkSum = await fs.readFile(checkSumPath, encoding);
+    if (checkSum !== getCheckSum(content)) {
+      console.error("Env file should be updated");
+      process.exit(1);
+    }
+  }, 0);
+} else {
+  watch(envPath).on("all", async () => {
+    const content = await fs.readFile(envPath, encoding);
+    const checkSum = getCheckSum(content);
+    fs.writeFile(checkSumPath, checkSum, encoding);
+  });
+}
