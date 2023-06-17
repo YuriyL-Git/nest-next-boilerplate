8e41d3ade669e80961348465a3e10933f28c38d1
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-06-17 19:35:44 +0300

    use nodejs runtime for revalidate

--- apps/web/app/next-api/revalidate/route.ts
+++ apps/web/app/next-api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get("path") || "/";
