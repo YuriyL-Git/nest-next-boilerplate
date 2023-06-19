7ced5f37a486aedb399773e91297e1c113668cd8
Yuriy Lipchey <yuriy.lipchey.work@gmail.com>
2023-06-19 11:17:16 +0300

    update pre-commit logic

--- tools/prisma-generator/prisma-generator.ts
+++ tools/prisma-generator/prisma-generator.ts
@@ -4,5 +4,7 @@ import { exec } from "child_process";
 const prismaSchema = "libs/api/data-access-db/src/lib/schema.prisma";
 
 watch(prismaSchema).on("change", async () => {
-  exec("npx prisma db push && npm run start:prisma-studio");
+  exec(
+    "npx env-cmd -f .local.env npx prisma db push --accept-data-loss && npm run start:prisma-studio",
+  );
 });
